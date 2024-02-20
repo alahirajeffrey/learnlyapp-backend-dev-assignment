74d69e945606bcdbb9be1b88f6dfa2f5dc3194b1
alahirajeffrey <alahirajeffrey@gmail.com>
2024-02-20 16:41:54 +0100

    setup validation and swagger document

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,43 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  // setup validation
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
+
+  // set global api prefix
+  app.setGlobalPrefix('api/v1');
+
+  // setup swagger
+  const config = new DocumentBuilder()
+    .setTitle('Learnly Backend Dev Assignment')
+    .setDescription('Learnly Backend Dev Assignment API description')
+    .setVersion('1.0')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        name: 'JWT',
+        description: 'Enter JWT token',
+        in: 'header',
+      },
+      'JWT-auth',
+    )
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-doc', app, document);
+
+  await app.listen(Number(process.env.NODE_PORT) || 3000);
 }
 bootstrap();
