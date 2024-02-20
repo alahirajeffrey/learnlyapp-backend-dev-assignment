c6ea42ca29448d81ce663d333f7ddcd7c57705b2
alahirajeffrey <alahirajeffrey@gmail.com>
2024-02-20 23:19:15 +0100

    register user

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { AuthService } from './auth.service';
+import { RegisterUserDto } from 'src/dtos/auth.dto';
 
+@ApiTags('auth-endpoints')
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('register')
+  @ApiOperation({ summary: 'Register a user' })
+  registerPatient(@Body() dto: RegisterUserDto) {
+    return this.authService.registerUser(dto);
+  }
+}
