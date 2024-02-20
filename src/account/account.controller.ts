a9e05dcc34a8486624b8240bd780a72e4bd32c65
alahirajeffrey <alahirajeffrey@gmail.com>
2024-02-21 00:13:51 +0100

    added pagination dto

--- src/account/account.controller.ts
+++ src/account/account.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, Param, Post, Req } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Req } from '@nestjs/common';
 import { AccountService } from './account.service';
 import { ApiOperation, ApiSecurity, ApiTags } from '@nestjs/swagger';
+import { PaginationDto } from 'src/dtos/pagination.dto';
 
 @ApiSecurity('JWT-auth')
 @ApiTags('account-endpoints')
@@ -20,16 +21,19 @@ export class AccountController {
     return this.accountService.getOwnAccountDetails(req.user.email);
   }
 
-  // protect with roles guard
+  // protect with admin roles guard
   @Get('transactions/other/:accountNumber')
   @ApiOperation({ summary: 'Get other`s account transactions' })
-  getOtherAccountTransactions(@Param('accountNumber') accountNumber: string) {
-    return this.accountService.getOtherAccountTransactions(accountNumber);
+  getOtherAccountTransactions(
+    @Param('accountNumber') accountNumber: string,
+    @Body() dto: PaginationDto,
+  ) {
+    return this.accountService.getOtherAccountTransactions(accountNumber, dto);
   }
 
   @Get('transactions/own')
   @ApiOperation({ summary: 'Get own account transactions' })
-  getOwnAccountTransactions(@Req() req) {
-    return this.accountService.getOwnAccountTransactions(req.user.email);
+  getOwnAccountTransactions(@Req() req, dto: PaginationDto) {
+    return this.accountService.getOwnAccountTransactions(req.user.email, dto);
   }
 }
