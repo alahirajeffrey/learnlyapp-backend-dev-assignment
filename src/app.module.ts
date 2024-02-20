89397654935286127a636d22167c958cdf9de7bc
alahirajeffrey <alahirajeffrey@gmail.com>
2024-02-20 16:36:45 +0100

    setup rate limiter

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
-  imports: [],
+  imports: [
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60,
+        limit: 10,
+      },
+    ]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
