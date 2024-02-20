3c4eae9cf6ad1b8cce12fd3de4d8210443232761
alahirajeffrey <alahirajeffrey@gmail.com>
2024-02-20 22:52:39 +0100

    setup jwt and mongofb

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { config } from 'src/common/config/confg';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Account, AccountSchema } from 'src/schemas/account.schema';
 
 @Module({
+  imports: [
+    JwtModule.register({
+      global: true,
+      secret: config.JWT_SECRET,
+      signOptions: { expiresIn: '10m' },
+    }),
+    MongooseModule.forFeature([{ name: Account.name, schema: AccountSchema }]),
+  ],
   providers: [AuthService],
-  controllers: [AuthController]
+  controllers: [AuthController],
 })
 export class AuthModule {}
