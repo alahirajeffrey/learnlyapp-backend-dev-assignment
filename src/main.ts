0cd1f0762237429a831d91e2a269e0f12c834fe1
alahirajeffrey <alahirajeffrey@gmail.com>
2024-02-20 21:59:21 +0100

    setup helmet

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -37,6 +38,9 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api-doc', app, document);
+
+  // setup helmet for security
+  app.use(helmet());
 
   await app.listen(Number(process.env.NODE_PORT) || 3000);
 }
