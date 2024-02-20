8f6e6774370d3c13617be8a22e3c6b6a321b2349
alahirajeffrey <alahirajeffrey@gmail.com>
2024-02-21 00:04:56 +0100

    added account controllers

--- src/account/account.controller.ts
+++ src/account/account.controller.ts
@@ -1,4 +1,35 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Post, Req } from '@nestjs/common';
+import { AccountService } from './account.service';
+import { ApiOperation, ApiSecurity, ApiTags } from '@nestjs/swagger';
 
+@ApiSecurity('JWT-auth')
+@ApiTags('account-endpoints')
 @Controller('account')
-export class AccountController {}
+export class AccountController {
+  constructor(private readonly accountService: AccountService) {}
+
+  @Post('create')
+  @ApiOperation({ summary: 'Create an account for a user' })
+  createAccount(@Req() req) {
+    return this.accountService.createAccount(req.user.email);
+  }
+
+  @Get('own')
+  @ApiOperation({ summary: 'Get own account details' })
+  getOwnAccountDetails(@Req() req) {
+    return this.accountService.getOwnAccountDetails(req.user.email);
+  }
+
+  // protect with roles guard
+  @Get('transactions/other/:accountNumber')
+  @ApiOperation({ summary: 'Get other`s account transactions' })
+  getOtherAccountTransactions(@Param('accountNumber') accountNumber: string) {
+    return this.accountService.getOtherAccountTransactions(accountNumber);
+  }
+
+  @Get('transactions/own')
+  @ApiOperation({ summary: 'Get own account transactions' })
+  getOwnAccountTransactions(@Req() req) {
+    return this.accountService.getOwnAccountTransactions(req.user.email);
+  }
+}
