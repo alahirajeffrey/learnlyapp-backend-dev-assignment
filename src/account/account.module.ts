68d5cd6c2cb11d6c477a65ca0c7aad8acc3f0193
alahirajeffrey <alahirajeffrey@gmail.com>
2024-02-20 23:33:32 +0100

    setup account module

--- src/account/account.module.ts
+++ src/account/account.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AccountService } from './account.service';
 import { AccountController } from './account.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Account, AccountSchema } from 'src/schemas/account.schema';
+import { User, UserSchema } from 'src/schemas/user.schema';
+import { Transaction, TransactionSchema } from 'src/schemas/transaction.schema';
 
 @Module({
   providers: [AccountService],
-  controllers: [AccountController]
+  controllers: [AccountController],
+  imports: [
+    JwtModule.register({}),
+    MongooseModule.forFeature([
+      { name: Account.name, schema: AccountSchema },
+      { name: User.name, schema: UserSchema },
+      { name: Transaction.name, schema: TransactionSchema },
+    ]),
+  ],
 })
 export class AccountModule {}
