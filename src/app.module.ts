368cdf00dca83e2d31202a1e29c5c6c154140f64
alahirajeffrey <alahirajeffrey@gmail.com>
2024-02-20 16:49:31 +0100

    setup condig, mongodb and auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,15 +2,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ThrottlerModule } from '@nestjs/throttler';
+import { AuthModule } from './auth/auth.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+import { config } from './common/config/confg';
 
 @Module({
   imports: [
+    // set rate limiter to 10 reqs per second
     ThrottlerModule.forRoot([
       {
         ttl: 60,
         limit: 10,
       },
     ]),
+    AuthModule,
+    MongooseModule.forRoot(config.MONGO_URI),
+    ConfigModule.forRoot({ isGlobal: true }),
   ],
   controllers: [AppController],
   providers: [AppService],
