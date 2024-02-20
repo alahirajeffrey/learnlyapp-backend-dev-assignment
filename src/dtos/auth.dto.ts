f961aa52a228c9eaf15d24e4096737d45a489c6c
alahirajeffrey <alahirajeffrey@gmail.com>
2024-02-20 22:04:23 +0100

    added dto to register user

---
+++ src/dtos/auth.dto.ts
@@ -0,0 +1,49 @@
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+export class RegisterUserDto {
+  @IsString()
+  @IsNotEmpty()
+  @IsEmail()
+  @ApiProperty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty()
+  firstName: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty()
+  mobileNumber: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty()
+  lastName: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty()
+  @MinLength(8, {
+    message:
+      'Password is too short. Minimal length is $constraint1 characters, but actual is $value',
+  })
+  @MaxLength(20, {
+    message:
+      'password is too long. Maximal length is $constraint1 characters, but actual is $value',
+  })
+  @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?!.*\s).{8,20}$/, {
+    message:
+      'password must contain the following: a capital letter, a small letter, and a number',
+  })
+  password: string;
+}
