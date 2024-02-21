179ed9e25a10cb4db5e750d413e73fac0108560b
alahirajeffrey <alahirajeffrey@gmail.com>
2024-02-21 16:32:06 +0100

    controllers to deposit, withdraw and transfer funds

---
+++ src/transaction/transaction.controller.ts
@@ -0,0 +1,29 @@
+import { Body, Controller, Patch, Req } from '@nestjs/common';
+import { ApiOperation, ApiSecurity, ApiTags } from '@nestjs/swagger';
+import { TransactionService } from './transaction.service';
+import { DepositAndWithdrawalDto, TransferDto } from 'src/dtos/transaction.dto';
+
+@ApiSecurity('JWT-auth')
+@ApiTags('transaction-endpoints')
+@Controller('transaction')
+export class TransactionController {
+  constructor(private readonly transactionService: TransactionService) {}
+
+  @Patch('deposit')
+  @ApiOperation({ summary: 'Fund account' })
+  depositFunds(@Req() req, @Body() dto: DepositAndWithdrawalDto) {
+    return this.transactionService.depositFunds(req.user.email, dto);
+  }
+
+  @Patch('transfer')
+  @ApiOperation({ summary: 'Transfer funds' })
+  transferFunds(@Req() req, @Body() dto: TransferDto) {
+    return this.transactionService.transferFunds(req.user.email, dto);
+  }
+
+  @Patch('withdrawal')
+  @ApiOperation({ summary: 'Withdraw funds' })
+  withdrawFunds(@Req() req, @Body() dto: DepositAndWithdrawalDto) {
+    return this.transactionService.withdrawFunds(req.user.email, dto);
+  }
+}
