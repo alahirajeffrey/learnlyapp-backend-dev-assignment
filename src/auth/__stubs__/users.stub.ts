a3d508c77a106038ec8a23b069bcaf2b1f48b035
alahirajeffrey <alahirajeffrey@gmail.com>
2024-02-22 15:17:13 +0100

    added user stubs

---
+++ src/auth/__stubs__/users.stub.ts
@@ -0,0 +1,19 @@
+export const firstUserStub = () => {
+  return {
+    firstName: 'jeffrey',
+    lastName: 'alahira',
+    email: 'jeffreyalahira@email.com',
+    password: 'P@ssw0rd',
+    mobileNumber: '1234567890',
+  };
+};
+
+export const secondUserStub = () => {
+  return {
+    firstName: 'alahira',
+    lastName: 'jeffrey',
+    email: 'alahirajeffrey@email.com',
+    password: 'P@ssw0rd',
+    mobileNumber: '1234567890',
+  };
+};
